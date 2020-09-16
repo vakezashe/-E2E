86ca1c6c7207193b057c0f2a2e209744762b5264
mye <vakezashe13@mail.ru>
2020-09-16 22:21:14 +0300

    a third commit

--- authorization_page_welcome_back.py
+++ authorization_page_welcome_back.py
@@ -1,13 +1,7 @@
-# from gevent import os
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import requests
-
-
 from e2e import users_pom
 
 # user = 'wrong_user'
@@ -15,10 +9,13 @@ from e2e import users_pom
 # print(users_pom.get_user(user)["email"] and users_pom.get_user(user)["password"])
 
 
-def routes(user):
-
+def wdriver():
     driver = webdriver.Firefox(executable_path=r'./geckodriver')
     driver.get('https://www.sbzend.ssls.com')
+    return driver
+
+
+def routes(user, driver):
 
     WebDriverWait(driver, 5).until(
         EC.visibility_of_element_located((By.XPATH, "//span[@class='ssls-toolbar__btn-text']"))).click()
@@ -37,22 +34,16 @@ def routes(user):
 
     WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//a[@href='/user/profile']"))).click()
 
-# one = browser.find_element_by_xpath('/ins').text
-
     WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//span[@class='text ng-binding']")))
 
-    # qwe = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//span[@class='text ng-binding']"))).text()
+    name = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//span[@ng-hide=\"activeRow === 'name'\"]"))).text
+    email = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//span[@ng-hide=\"activeRow === 'email'\"]"))).text
+    password = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//span[@ng-hide=\"activeRow === 'password'\"]"))).text
+    phone = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//span[@ng-hide=\"activeRow === 'phone'\"]"))).text
+    address = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//span[@ng-hide=\"activeRow === 'address'\"]"))).text
 
-    # WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//span[@ng-hide=\"activeRow === 'name'\"]")))
-
-    # WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//span[@class='icon icon-pencil']"))).click()
-
-    WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//span[@class='text ng-binding']"))).click()
-
-    # # one = driver.find_element_by_xpath("//span[@class='text ng-binding']").text
-    # one = driver.find_element_by_xpath('div[@class="item"]')
-    # # print(one)
+    print(name,'\n',email,'\n',address,'\n',password,'\n',phone,'\n',address)
 
 
 if __name__ == "__main__":
-    routes(user='correct_user')
+    routes(user='correct_user', driver=wdriver())
