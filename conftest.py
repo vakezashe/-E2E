9ca001b03e11543c01e136bbf04694383d81a8d7
mye <vakezashe13@mail.ru>
2020-11-14 19:05:01 +0200

    add headless method

--- conftest.py
+++ conftest.py
@@ -1,10 +1,18 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 
 @pytest.fixture(scope="session")
 def browser():
     # driver = webdriver.Chrome(executable_path="./chromedriver")
-    driver = webdriver.Firefox(executable_path=r'./geckodriver')
+    # driver = webdriver.Firefox(executable_path=r'./geckodriver')
+
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options, executable_path=r'./geckodriver')
     yield driver
-    # driver.quit()
+    driver.quit()
+
+
+
